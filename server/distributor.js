c247bfe16b347dcf974d63fa8c5a0a86e7c6ebd6
Jack <j.jobs1028@gmail.com>
2018-06-13 11:00:50 -0700

    Add distributor.js
    Distributor communicate each other with their address information.

---
+++ server/distributor.js
@@ -0,0 +1,101 @@
+'use strict'
+/**
+ * @description Tcp Server Object distributor
+ * @author Junhee Park (j.jobs1028/gmail.com, Qualcomm Institute)
+ * @since       2018. 06. 12.
+ * @last update 2018. 06. 12.
+ */
+
+ var map = {};
+ /**
+  * Extends tcpServer class to distributor class
+  * 
+  * Extends tcpServer class to distrbutor class. distributor exist for share each clients address.
+  * When create a socket, the address and port information stored in the map object. 
+  * 
+  */
+
+/**
+ * Define param protocol
+ * 
+ * [
+ *    {
+ *        "port": "port of the first node",
+ *        "name": "name of the first node",
+ *        "urls": [
+ *            "first url of the first node",
+ *            "second url of the first node"
+ *            ...
+ *        ],
+ *        "host": "host of the first node"
+ *    },
+ *  {
+ *        "port": "port of the second node",
+ *        "name": "name of the second node",
+ *        "urls": [
+ *            "first url of the second node",
+ *            "second url of the sencond node"
+ *            ...
+ *        ],
+ *        "host": "host of the sencond node"
+ *    },
+ *    ...
+ * ]
+ */
+ class distributor extends require('./server.js') {
+     constructor() {
+        super("distributor", 9000, ["POST/distributor", "GET/distributor"]); // (?) super
+     }
+
+     onCreate(socket) {
+         console.log("onCreate", socket.remoteAddess, socket.remotePort);
+         this.sendInfo(socket);
+     }
+
+     onClose(socket) {
+         var key = socket.remoteAddess + ":" + socket.remotePort;
+         console.log("onClose", socket.remoteAddess, socket.remotePort);
+         delete map[key];
+         this.sendInfo();
+     }
+
+     onRead(socket, json) {
+         var key = socket.remoteAddess + ":" + socket.remotePort;
+         console.log("OnRead", socket.remoteAddess, socket.remotePort, json);
+
+         if(json.uri == "distributes" && json.method == "POST") {
+             map[key] = {
+                 socket: socket
+             };
+             map[key].info = jsonParam;
+             map[key].info.host = socket.remoteAddess;
+             this.sendInfo();
+         }
+     }
+
+     write(socket, packet) {
+         socket.write(JSON.stringify(packet) + ' ');
+     }
+
+     sendInfo(socket) {
+         var packet = {
+             uri: "/distributes",
+             method: "GET",
+             key: 0,
+             params: []
+         };
+         for (var n in map) {
+             packet.params.push(map[n].info);
+         }
+
+         if (socket) {
+             this.write(socket, packet);
+         } else {
+             for (var n in map) {
+                 this.write(map[n].socket, packet);
+             }
+         }
+     }
+ }
+
+ new distributor();
---
+++ server/server.js
@@ -0,0 +1,103 @@
+'use strict'
+/**
+ * @description Tcp Server Object
+ * @author Junhee Park (j.jobs1028/gmail.com, Qualcomm Institute)
+ * @brief net documentation: https://nodejs.org/api/net.html
+ * @since       2018. 06. 12.
+ * @last update 2018. 06. 12.
+ * +----------------------+
+ * |      tcpServer       |
+ * +----------------------+
+ * |tcpClient: tcpClient  |
+ * |server: net           |
+ * +----------------------+
+ * |connectToDistributor()|
+ * +----------------------+
+ */
+
+const net = require('net');
+const tcpClient = require('./client.js');
+//Define class tcpServer
+class tcpServer {
+    //Define constructor
+    constructor(name, port, urls) {
+        this.context = {    //Server information
+            port: port,
+            name: name,
+            urls: urls
+        }
+        this.merge = {};
+
+        this.server = net.createServer((socket) => {    //Create server
+            this.onCreate(socket);  // (?) net.onCreate
+
+            socket.on('error', (exception) => {
+                this.onClose(socket);
+            });
+            socket.on('close', (exception) => {
+                this.onClose(socket);
+            });
+            socket.on('data', (data) => {
+                var key = socket.remoteAddress + ':' + socket.remotePort;
+                var sz = this.merge[key] ? this.merge[key] + data.toString() :
+                         data.toString();
+                for (var n in arr) { // (?)
+                    if (sz.charAt(sz.length - 1) != ' ' && n == arr.length - 1) {
+                        this.merge[key] = arr[n];
+                        break;
+                    } else if (arr[n] == ""){
+                        break;
+                    } else {
+                        this.onRead (socket, JSON.parse(arr[n]));
+                    }
+                }
+            });
+        });
+
+        this.server.on('error', (err) => {
+            console.log(err);
+        });
+
+        this.server.listen(port, () => {
+            console.log('listen', this.server.address());
+        });
+    }
+
+    onCreate(socket) {
+        console.log("onCreate", socket.remoteAddress, socket.remotePort);
+    }
+    
+    onClose(socket){
+        console.log("onClose", socket.remoteAddress, socket.remotePort);
+    }
+
+    connectToDistributor(host, port, onNoti) {  //Micro services use this function
+        var packet = {
+            uri: "/distributes",
+            method: "POST",
+            key: 0,
+            param: this.context
+        };
+        var isConnectedDistributor = false;
+
+        this.clientDistributor = new tcpClient (
+            host
+            , port
+            , (options) => {
+                isConnectedDistributor = true;
+                this.clientDistributor.write(packet);
+            }
+            , (options, data) => { onNoti(data); }
+            , (options) => { isConnectedDistributor = false; }
+            , (options) => { isConnectedDistributor = false; }
+        );
+
+        setInterval(() => { // Retry connection with distributor
+            if (isConnectedDistributor != true) {
+                this.clientDistributor.connect();
+            }
+        }, 3000);
+    }
+}
+
+module.exports = tcpServer;
