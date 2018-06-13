a732440c0eb98d811f0965832b23d0e668a56bf3
Jack <j.jobs1028@gmail.com>
2018-06-12 17:28:54 -0700

    Install basic modules for web service
    npm install http (v0.0.0)
    npm install net (v1.0.2)

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 /**
  * @file index.js
- * @author Junhee Park (j.jobs1028@gmail.com)
+ * @author Junhee Park (j.jobs1028@gmail.com, Qualcomm Institute)
  * @brief This project is a tutorial for the Microservice architecture based on Node.js
+ * @since       2018. 06. 12.
+ * @last update 2018. 06. 12.
+ * @version 0.0 
  */
+
+//Install basic modules for web service
