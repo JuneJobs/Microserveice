df40e5919cc73e9b6af332fa454a494faa358708
Jack <j.jobs1028@gmail.com>
2018-06-15 17:27:21 -0700

    Add execute shells
    Added shells whitch work for execute node process in this system.

---
+++ execute/distributor.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd /Users/junheepark/Desktop/Stuff/Technique/NodeJs/Microservice/server/server_modules
+
+node distributor.js 
---
+++ execute/microservice.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd /Users/junheepark/Desktop/Stuff/Technique/NodeJs/Microservice/server/controller
+
+node microservice_goods.js 
